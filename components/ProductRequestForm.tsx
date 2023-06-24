7ce34b68591f2a9559ee4e53068b5fa8c2857917
Anant Shanker <anant@MacBook-Air-3.local>
2023-06-25 00:14:54 +0800

    updated prisma schema + fixed frontend

--- components/ProductRequestForm.tsx
+++ components/ProductRequestForm.tsx
@@ -14,13 +14,15 @@ export default function ProductRequestForm() {
         productCity: '',
         productURL: '',
         productName: '',
-        quantity: '',
-        yourOffer: '',
+        quantity: 0,
+        yourOffer: 0,
         name: '',
         deliveryCountry: '',
         deliveryState: '',
         deliveryCity: '',
         contactNumber: '',
+        imageURL:'',
+        productDetails:''
       });
 
       const handleSubmit = async (event:any) => {
@@ -59,8 +61,8 @@ export default function ProductRequestForm() {
     
       return (
         <form onSubmit={handleSubmit}>
-          <div>
-            <Row className="max-w-5xl mx-auto px-20 ml-20 grid grid-cols-2">
+          <div className=' bg-rose-100'>
+            <Row className="max-w-7xl px-20 grid grid-cols-2">
               <Col className='pt-20'>
                 <h1 className="sm:text-8xl text-blue-800 font-extrabold text-left whitespace-normal">
                   STEP 1:
@@ -70,45 +72,53 @@ export default function ProductRequestForm() {
                 </h1>
                 <h3 className="sm:text-2xl mt-2 text-green-700 font-semibold text-left break-words">
                   Do make sure all information is accurate, and include specifications
-                  (e.g size/color) under Product Name + Selection!
+                  (e.g size/color) under Product Name + Details!
                 </h3>
               </Col>
-              <Col className='pt-20 pl-52'>
+              <Col className='pt-20 pl-32'>
                 <Row className='grid grid-cols-3'>
                   <div className='pb-10'>
                     <h1>Product Country</h1>
-                    <input className='w-24 rounded' name="productCountry" value={formData.productCountry} onChange={handleChange} />
+                    <input className=' w-36 rounded' name="productCountry" value={formData.productCountry} onChange={handleChange} />
                   </div>
-                  <div className='pl-10 pb-10'>
+                  <div className='pl-4 pb-10'>
                     <h1>Product State</h1>
-                    <input className='w-24 rounded' name="productState" value={formData.productState} onChange={handleChange} />
+                    <input className='w-36 rounded' name="productState" value={formData.productState} onChange={handleChange} />
                   </div>
-                  <div className='pl-20 pb-10'>
+                  <div className='pl-8 pb-10'>
                     <h1>Product City</h1>
-                    <input className='w-24 rounded' name="productCity" value={formData.productCity} onChange={handleChange} />
+                    <input className='w-32 rounded' name="productCity" value={formData.productCity} onChange={handleChange} />
                   </div>
                 </Row>
                 <div className='pb-10'>
-                  <h1>Enter Product URL</h1>
-                  <input className='w-96 rounded' name="productURL" value={formData.productURL} onChange={handleChange} />
+                  <h1>Enter Product Web URL (Optional)</h1>
+                  <input className='w-[450px] rounded' name="productURL" value={formData.productURL} onChange={handleChange} />
                 </div>
                 <div className='pb-10'>
-                  <h1>Product Name + Selection</h1>
+                  <h1>Enter Image URL (Optional) </h1>
+                  <input className='w-[450px] rounded' name="imageURL" value={formData.imageURL} onChange={handleChange} />
+                </div>
+                <div className='pb-10'>
+                  <h1>Product Name</h1>
                   <input className='w-96 rounded' name="productName" value={formData.productName} onChange={handleChange} />
+                </div>
+                <div className='pb-10'>
+                  <h1>Product Details</h1>
+                  <input className='w-96 rounded' name="productDetails" value={formData.productDetails} onChange={handleChange} />
                 </div>
                 <Row className='grid grid-cols-2'>
                   <div className='pb-10'>
                     <h1>Quantity</h1>
-                    <input className='w-44 rounded' name="quantity" value={formData.quantity} onChange={handleChange} />
+                    <input className='w-44 rounded' type="number" name="quantity" value={formData.quantity} onChange={handleChange} />
                   </div>
-                  <div className='pl-20 pb-10'>
-                    <h1>YourOffer</h1>
-                    <input className='w-44 rounded' name="yourOffer" value={formData.yourOffer} onChange={handleChange} />
+                  <div className='pl-10 pb-10'>
+                    <h1>Your Offer (USD)</h1>
+                    <input className='w-44 rounded' type="number" name="yourOffer" value={formData.yourOffer} onChange={handleChange} />
                   </div>
                 </Row>
               </Col>
             </Row>
-            <Row className="max-w-5xl mx-auto px-20 ml-20 grid grid-cols-2">
+        <Row className="max-w-7xl px-20 grid grid-cols-2 pt-20">
           <Col className='pt-20'>
             <h1 className="sm:text-8xl text-blue-800 font-extrabold text-left whitespace-normal">
               STEP 2:
@@ -117,28 +127,28 @@ export default function ProductRequestForm() {
               DELIVERY DETAILS
             </h1>
           </Col>
-          <Col className='pt-20 pl-52'>
-            <div className='pb-10'>
+          <Col className='pt-20 pl-32'>
+            {/* <div className='pb-10'>
               <h1>Name</h1>
               <input className='w-96 rounded' name="name" value={formData.name} onChange={handleChange} />
-            </div>
+            </div> */}
             <Row className='grid grid-cols-3'>
               <div className='pb-10'>
                 <h1>Delivery Country</h1>
-                <input className='w-24 rounded' name="deliveryCountry" value={formData.deliveryCountry} onChange={handleChange} />
+                <input className='w-32 rounded' name="deliveryCountry" value={formData.deliveryCountry} onChange={handleChange} />
+              </div>
+              <div className='pl-5 pb-10'>
+                <h1>Delivery State</h1>
+                <input className='w-32 rounded' name="deliveryState" value={formData.deliveryState} onChange={handleChange} />
               </div>
               <div className='pl-10 pb-10'>
-                <h1>Delivery State</h1>
-                <input className='w-24 rounded' name="deliveryState" value={formData.deliveryState} onChange={handleChange} />
-              </div>
-              <div className='pl-20 pb-10'>
                 <h1>Delivery City</h1>
-                <input className='w-24 rounded' name="deliveryCity" value={formData.deliveryCity} onChange={handleChange} />
+                <input className='w-32 rounded' name="deliveryCity" value={formData.deliveryCity} onChange={handleChange} />
               </div>
             </Row>
             <div className='pb-10'>
               <h1>Contact Number</h1>
-              <input className='w-96 rounded' name="contactNumber" value={formData.contactNumber} onChange={handleChange} />
+              <input className='w-80 rounded' name="contactNumber" value={formData.contactNumber} onChange={handleChange} />
             </div>
           </Col>
         </Row>
@@ -152,99 +162,3 @@ export default function ProductRequestForm() {
     </form>
   );
 };
-
-    // return (
-    
-    //     <div>
-    //         <div> Welcome </div>
-    //         <Row className="max-w-5xl mx-auto px-20 ml-20 grid grid-cols-2">
-    //              <Col className='pt-20'>
-    //                  <h1 className="sm:text-8xl text-blue-800 font-extrabold text-left whitespace-normal">
-    //                      STEP 1: 
-    //                  </h1>
-    //                  <h1 className="sm:text-8xl mt-2 text-blue-800 font-extrabold text-left">
-    //                      PRODUCT DETAILS
-    //                  </h1>
-    //                  <h3 className="sm:text-2xl mt-2 text-green-700 font-semibold text-left break-words">
-    //                  Do make sure all information is accurate, and include specifications 
-    //                  (e.g size/color) under Product Name + Selection!
-    //                  </h3>
-    //              </Col>
-    //              <Col className='pt-20 pl-52'>
-    //                 <Row className='grid grid-cols-3'>
-    //                     <div className='pb-10'>
-    //                         <h1>Product Country</h1>
-    //                         <input className='w-24 rounded' />
-    //                     </div>
-    //                     <div className='pl-10 pb-10'>
-    //                         <h1>Product State</h1>
-    //                         <input className='w-24 rounded' />
-    //                     </div>
-    //                     <div className='pl-20 pb-10'>
-    //                         <h1>Product City</h1>
-    //                         <input className='w-24 rounded' />
-    //                     </div>
-    //                 </Row>
-    //                 <div className='pb-10'>
-    //                     <h1>Enter Product URL</h1>
-    //                     <input className='w-96 rounded' />
-    //                 </div>
-    //                 <div className='pb-10'>
-    //                     <h1>Product Name + Selection</h1>
-    //                     <input className='w-96 rounded' />
-    //                 </div>
-    //                 <Row className='grid grid-cols-2'>
-    //                     <div className='pb-10'>
-    //                         <h1>Quantity</h1>
-    //                         <input className='w-44 rounded' />
-    //                     </div>
-    //                     <div className='pl-20 pb-10'>
-    //                         <h1>YourOffer </h1>
-    //                         <input className='w-44 rounded' />
-    //                     </div>
-    //                 </Row>
-    //              </Col>
-    //         </Row>
-
-    //         <Row className="max-w-5xl mx-auto px-20 ml-20 grid grid-cols-2">
-    //              <Col className='pt-20'>
-    //                  <h1 className="sm:text-8xl text-blue-800 font-extrabold text-left whitespace-normal">
-    //                      STEP 2: 
-    //                  </h1>
-    //                  <h1 className="sm:text-8xl mt-2 text-blue-800 font-extrabold text-left">
-    //                      DELIVERY DETAILS
-    //                  </h1>
-    //              </Col>
-    //              <Col className='pt-20 pl-52'>
-    //                 <div className='pb-10'>
-    //                     <h1>Name</h1>
-    //                     <input className='w-96 rounded' />
-    //                 </div>
-    //                 <Row className='grid grid-cols-3'>
-    //                     <div className='pb-10'>
-    //                         <h1>Delivery Country</h1>
-    //                         <input className='w-24 rounded' />
-    //                     </div>
-    //                     <div className='pl-10 pb-10'>
-    //                         <h1>Delivery State</h1>
-    //                         <input className='w-24 rounded' />
-    //                     </div>
-    //                     <div className='pl-20 pb-10'>
-    //                         <h1>Delivery City</h1>
-    //                         <input className='w-24 rounded' />
-    //                     </div>
-    //                 </Row>
-    //                 <div className='pb-10'>
-    //                     <h1>Contact Number</h1>
-    //                     <input className='w-96 rounded' />
-    //                 </div>
-    //              </Col>
-    //         </Row>
-    //         <Row className="text-center py-20">
-    //             <button type="submit"
-    //             className='bg-green-700 hover:bg-green-900 text-white text-4xl font-bold py-2 px-4 rounded-lg'>
-    //                     Confirm and Submit
-    //                 </button>
-    //         </Row>
-    //     </div>
-    // );
