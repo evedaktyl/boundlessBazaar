e356bcaa7337bd9196b58642cfe2e7fe5b6e46ab
Anant Shanker <anant@MacBook-Air-3.local>
2023-06-25 20:44:07 +0800

    fixed successful request page

--- pages/ProductRequestPage/SuccessfulRequest.tsx
+++ pages/ProductRequestPage/SuccessfulRequest.tsx
@@ -7,7 +7,6 @@ function SuccessfulRequest() {
             <h1 className='text-blue-800 text-6xl font-bold text-center mt-10 mr-10'>
                 Success!
             </h1>
-            //to be linked to individual listing
             <button className='bg-green-700 hover:bg-green-900 text-white text-4xl font-bold py-2 px-4 rounded-lg mt-10'>
                 View Listing
             </button>
