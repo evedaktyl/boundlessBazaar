5abf2085e1763ea3be3ee7bf127d9500c6bdad82
Anant Shanker <anant@MacBook-Air-3.local>
2023-07-11 22:59:18 +0800

    got search component on marketplace to work

--- pages/MarketplacePage/index.tsx
+++ pages/MarketplacePage/index.tsx
@@ -10,22 +10,44 @@ export default function Marketplace() {
     const router = useRouter();
 
     const [searchedProductName, setSearchedProductName] = useState("");
-  const [purchaseCountry, setPurchaseCountry] = useState("");
-  const [deliveryCountry, setDeliveryCountry] = useState("");
-  const [purchaseState, setPurchaseState] = useState("");
-  const [deliveryState, setDeliveryState] = useState("");
+    const [purchaseCountry, setPurchaseCountry] = useState("");
+    const [deliveryCountry, setDeliveryCountry] = useState("");
+    const [purchaseState, setPurchaseState] = useState("");
+    const [deliveryState, setDeliveryState] = useState("");
 
     const productNameHandler = (e: any) => {
         setSearchedProductName(e.currentTarget.value);
     }
+    const purchaseCountryHandler = (e: any) => {
+        setPurchaseCountry(e.currentTarget.value);
+    }
+    const deliveryCountryHandler = (e: any) => {
+        setDeliveryCountry(e.currentTarget.value);
+    }
+    const purchaseStateHandler = (e: any) => {
+        setPurchaseState(e.currentTarget.value);
+    }
+    const deliveryStateHandler = (e: any) => {
+        setDeliveryState(e.currentTarget.value);
+    }
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        searchListing(searchedProductName, purchaseCountry, purchaseState,
+                        deliveryCountry, deliveryState);
+    }
     const [products, changeProducts] = useState<any>([]);
+    const [searchedProducts, changeSearchedproducts] = useState<any>([]);
+    const [searchResultComponent, searchResultComponentChanged] = useState<any>();
+
     const callProducts = async () => {
         try {
             const response = await fetch('/api/getProducts', {
-                method: 'GET',
+                method: 'POST',
                 headers: {
                 'Content-Type': 'application/json',
-                }
+                },
+                body: JSON.stringify({})
             });
             if (response.ok) {
                 changeProducts(await response.json());
@@ -38,6 +60,83 @@ export default function Marketplace() {
         
     }
     callProducts();
+
+    const searchListing = async (title: string, purchaseCountry: string, purchaseState: string,
+                                deliverCountry:string, deliveryState:string) => {
+        try {
+            
+            const response = await fetch('/api/getProducts', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    title: title,
+                    collect_country: purchaseCountry,
+                    collect_state: purchaseState,
+                    deliver_country: deliverCountry,
+                    delivery_state: deliveryState
+                })
+            });
+            
+            if (response.ok) {
+                changeSearchedproducts(await response.json());
+
+                searchResultComponentChanged( 
+                <div>
+                    <div className='text-blue-800 text-6xl leading-8 font-bold text-left pt-20 pb-10 ml-[15%]'>
+                        Searched product listings:
+                    </div>
+                    <div className="grid grid-cols-2 mx-[15%] gap-2">
+                    {searchedProducts.map((product: {
+                        deliver_country:string,
+                        collect_country:string,
+                        quantity: number;
+                        curr_offer: number;
+                        title: string;
+                        image_url: string; id: React.Key | null | undefined; 
+                    }) => 
+                        <div className='mb-10 w-[480px] h-64 bg-rose-300 rounded-lg grid grid-cols-2 gap-1'
+                            key={product.id}>
+                        <Col className="ml-5 w-40">
+                            <div className="w-40 h-40 bg-white rounded-lg mt-5">
+                                <div className="w-[200px] h-[200px]">
+                                <Image src={"/trolley.png"}
+                                placeholder={'blur'}
+                                blurDataURL={"/public/BB_icon"}
+                                alt="no valid img url"
+                                className='pt-6' width={160} height={160} />
+                                </div>
+                            </div>
+                            <br />
+                            <div>
+                                {smallText(product.collect_country, 6)} {'->'} {smallText(product.deliver_country, 6)}
+                            </div>
+                        </Col>
+                        
+                        <Col className="mx-1 rounded-lg mt-5 text-2xl">
+                            <h1 className="pb-4 font-semibold">{smallText(product.title, 15)}</h1>
+                            <h1 className="pb-2">Offer Price: ${smallNum(product.curr_offer)}</h1>
+                            <h1 className="pb-10">Quantity Offered: {smallNum(product.quantity)}</h1>
+                            <button type="submit"
+                            className='bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-lg'
+                            onClick={() => handleOffer(event, product.id)}>
+                            Accept Offer
+                            </button>
+                        </Col>
+                    </div>
+                    )}
+                    </div>
+                </div>
+                );
+            } else {
+                const data:any = await response.json();
+                console.error(data.message);
+            }
+        } catch(error) {
+        }
+        
+    }
 
     const handleOffer = async (e:any, productID:any) => {
         if (session.status !== 'authenticated') {
@@ -94,7 +193,8 @@ export default function Marketplace() {
                     className='rounded-lg text-black text-1xl py-1 px-3 w-[420px]'  />
                     <a className="pl-8">
                         <button type="submit"
-                        className='bg-green-700 hover:bg-green-900  text-white font-bold py-2 px-6 rounded-full'>
+                        className='bg-green-700 hover:bg-green-900  text-white font-bold py-2 px-6 rounded-full'
+                        onClick={handleSubmit}>
                             Submit
                         </button>
                     </a>
@@ -143,8 +243,9 @@ export default function Marketplace() {
                 </Col>
              </Row>
             </div>
+            {searchResultComponent}
             <div className='text-blue-800 text-6xl leading-8 font-bold text-left pt-20 pb-10 ml-[15%]'>
-                Current Listings
+                All product listings:
             </div>
             <div className="grid grid-cols-2 mx-[15%] gap-2">
             {products.map((product: {
--- pages/api/getProducts.ts
+++ pages/api/getProducts.ts
@@ -5,7 +5,30 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ) {
+    const title = (req.body.title != null || undefined) ? req.body.title : '';
+    const collect_country = (req.body.collect_country != null || undefined) ? req.body.collect_country : '';
+    const collect_state = (req.body.collect_state != null || undefined) ? req.body.collect_state : '';
+    const deliver_country = (req.body.deliver_country != null || undefined) ? req.body.deliver_country : '';
+    const deliver_state = (req.body.deliver_state != null || undefined) ? req.body.deliver_state : '';
+
     const products = await prisma.products.findMany({
+        where: {
+            title: {
+                contains: title
+            },
+            collect_country: {
+                contains: collect_country
+            },
+            collect_state: {
+                contains: collect_state
+            },
+            deliver_country: {
+                contains: deliver_country
+            },
+            deliver_state: {
+                contains: deliver_state
+            },
+        },
     })
     res.status(200).json(products);
 }
