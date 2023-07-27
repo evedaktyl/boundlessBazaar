8584aab636f74a1046baf16ac47bd3edb2806fc7
Anant Shanker <anant@MacBook-Air-3.local>
2023-07-28 00:02:04 +0800

    edited product listing success page

--- pages/ProductRequestPage/SuccessfulRequest.tsx
+++ pages/ProductRequestPage/SuccessfulRequest.tsx
@@ -1,15 +1,16 @@
 'use client';
+import Link from "next/link";
 import React from "react";
 
 function SuccessfulRequest() {
     return (
-        <div className="flex justify-center items-center">
-            <h1 className='text-blue-800 text-6xl font-bold text-center mt-10 mr-10'>
+        <div className="ml-[10%]">
+            <h1 className='text-blue-800 text-6xl font-bold mb-5'>
                 Success!
             </h1>
-            <button className='bg-green-700 hover:bg-green-900 text-white text-4xl font-bold py-2 px-4 rounded-lg mt-10'>
-                View Listing
-            </button>
+            <Link href="/profile" className="mt-2 text-blue-800 text-2xl font-semibold pb-20 text-left underline">
+                Head over to your profile to see your product listed on the profile page.
+            </Link>
         </div>
     )
 }
